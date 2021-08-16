5dc45ae7d1c50881b989c499c96e254f0921fd88
Pierre-Loup Martin <lelabodutroisieme@gmail.com>
2021-08-16 14:15:09 +0200

    PPM and S.bus in ok. Added readme.

--- espRCControls.h
+++ espRCControls.h
@@ -1,3 +1,21 @@
+/*
+ * This program is part an esp32 library to read and write RC protocols
+ * Copyright (C) 2021  Pierre-Loup Martin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef ESP_RC_CONTROLS_H
 #define ESP_RC_CONTROLS_H
 
@@ -29,6 +47,9 @@ public:
 	controlData_t* getChannels();
 	int16_t getChannel(uint8_t channel);
 	uint8_t getNumberChannels();
+	
+	int16_t getResolution();
+	int16_t getNeutral();
 
 private:
 
---
+++ readme.md
@@ -0,0 +1,16 @@
+This library is for interfacing an esp32 with RC systems.
+
+It can read from transmitter, receiver, as well as to write to other devices, using the specific protocols used on RC systems.
+It's in its developpement phase, so for now it's a work in progress.
+
+Read from device :
+*PPM	- working
+*S.bus	- working
+
+Write to device :
+*PPM	- todo
+*S.bus	- todo
+
+It can also read or write telemetry using the following protocols :
+*Smart port 	- todo
+*F.port 		- todo
